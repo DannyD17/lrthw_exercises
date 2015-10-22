b0f7c8d6d0f0f8de481947cf67884f6f3646c5e5
Danny Debevec <daniel.debevec@gmail.com>
2015-10-22 11:55:16 -0600

    Added chapter 7 readme and ruby file
    [#104389400]

---
+++ Chapter_7/Readme.md
@@ -0,0 +1,4 @@
+# puts vs print
+
+> Both print and puts convert the information they are given to a string and output that information to the user.
+  Where they differ though it puts includes a newline character at the end of the string and print does not.
---
+++ Chapter_7/ex7.rb
@@ -0,0 +1,27 @@
+# Prints a string
+puts 'Mary had a little lamb.'
+# prints a string with another string interpolated in it
+puts "It's fleece was white as #{'snow'}."
+# prints a string
+puts 'And everywhere that Mary went.'
+# prints the string 10 times
+puts '.' * 10
+
+# Variable assignments
+end1 = 'C'
+end2 = 'h'
+end3 = 'e'
+end4 = 'e'
+end5 = 's'
+end6 = 'e'
+end7 = 'B'
+end8 = 'u'
+end9 = 'r'
+end10 = 'g'
+end11 = 'e'
+end12 = 'r'
+
+# print does not include a newline at the end.  Adds all the variables.
+print end1 + end2 + end3 + end4 + end5 + end6
+# puts includes a newline character at the end of the line.
+puts end7 + end8 + end9 + end10 + end11 + end12
